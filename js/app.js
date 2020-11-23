5839c52632b4c8b5b92d95cae10a6c5bb11c8753
cristina-q <cristinamihaela@yandex.com>
2020-11-24 01:09:49 +0200

    freez-time () ready

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ if (currentColor) {
   document.querySelector(':root').style.setProperty('--main-color', currentColor);
 }
 
-const time = document.getElementById('time');
+const time = document.querySelector('#time');
 const weekday = document.getElementById('dayname');
 const month = document.getElementById('month');
 const daynumber = document.getElementById('daynumber');
@@ -59,11 +59,10 @@ function displayTime() {
   daynumber.textContent = `${daynow}`;
   year.textContent = `${yearnow}`;
 }
-
 displayTime();
-setInterval(displayTime, 1000);
+let startDisplayTime = setInterval(displayTime, 1000);
 
-//************************* change page accent color
+//--------------  change page accent color  ----------
 const colorchange = document.querySelector('.change-color');
 
 colorchange.addEventListener('click', () => {
@@ -72,8 +71,9 @@ colorchange.addEventListener('click', () => {
   localStorage.setItem(`setColor`, `${setcurrentColor}`);
 });
 
-//************************* reuse functions
-
+// --------------------------------------------------
+//                 reuse functions
+//---------------------------------------------------
 function formatDigits(digit) {
   if (digit < 10) {
     digit = '0' + digit;
@@ -88,4 +88,56 @@ function getRandomColor() {
     color += chars[Math.floor(Math.random() * 16)];
   }
   return color;
+}
+
+//--------------  freez time / alarm  ----------------------
+
+const btnFreez = document.querySelector('.freez');
+const btnAlarm = document.querySelector('.alarm');
+
+btnFreez.addEventListener('click', clickHandler);
+
+function clickHandler(e) {
+  clearInterval(startDisplayTime);
+  let startfreez = new Date().getTime();
+  time.textContent = 'FREEZ';
+  document.querySelector('.freez-time').textContent = 'FREEZ ON';
+  document.querySelector('.freez-time').style.color = 'var(--main-color)';
+  document.querySelector('.alarm-time').textContent = 'FREEZ FOR';
+
+  btnAlarm.addEventListener('click', (e) => {
+    document.querySelector('.alarm-time').style.color = 'var(--main-color)';
+    let stopfreez = new Date().getTime();
+    let difference = Math.abs(startfreez - stopfreez);
+
+    time.textContent = getTimeDifference(difference);
+    btnFreez.removeEventListener('click', clickHandler);
+    document.querySelector('.freez-time').textContent = 'to CLOCK';
+
+    btnAlarm.addEventListener('click', (e) => {
+      document.querySelector('.alarm-time').textContent = 'STILL COUNT';
+      e.preventDefault();
+    });
+
+    e.preventDefault();
+  });
+
+  e.preventDefault();
+}
+
+function getTimeDifference(diff) {
+  let diffInHrs = diff / 3600000;
+  let hours = Math.floor(diffInHrs);
+
+  let diffInMin = (diffInHrs - hours) * 60;
+  let minutes = Math.floor(diffInMin);
+
+  let diffInSec = (diffInMin - minutes) * 60;
+  let seconds = Math.floor(diffInSec);
+
+  let formattedhours = hours.toString().padStart(2, '0');
+  let formattedminutes = minutes.toString().padStart(2, '0');
+  let formattedseconds = seconds.toString().padStart(2, '0');
+
+  return `${formattedhours} : ${formattedminutes} : ${formattedseconds}`;
 }
